580ded660016829519529c140174eeec103823f7
Payton Swick <payton@foolord.com>
2012-12-16 15:29:50 -0500

    Add more reordering tests

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -33,14 +33,27 @@ describe Trip do
     @trip.distance.should eq total_dist
   end
 
-  it "allows the Locations to be reordered" do
-    @trip.add_location @loc1 # 0: home
-    @trip.add_location @loc2 # 1: work
-    @trip.add_location @loc1 # 2: home
-    @trip.add_location @loc3 # 3: food
-    @trip.move_location(3, to: 1)
-    @trip.reload
-    @trip.location_at(1).title.should eq @loc3.title # 1: was work, now food
+  context "when Locations are moved" do
+    before do
+      @trip.add_location @loc1 # 0: home
+      @trip.add_location @loc2 # 1: work
+      @trip.add_location @loc1 # 2: home
+      @trip.add_location @loc3 # 3: food
+      @trip.move_location(3, to: 1)
+      @trip.reload
+    end
+
+    it "puts the new location at the right index" do
+      @trip.location_at(1).title.should eq @loc3.title # 1: was work, now food
+    end
+
+    it "moves the other locations down" do
+      @trip.location_at(2).title.should eq @loc2.title
+    end
+
+    it "does not affect higher locations" do
+      @trip.location_at(0).title.should eq @loc1.title
+    end
   end
 
   context "when adding a Location to an index" do
