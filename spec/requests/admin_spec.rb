deedd42507f355da654eec2bba0732224d950514
Payton Swick <payton@foolord.com>
2013-01-23 00:43:21 -0500

    Fix tests

--- spec/requests/admin_spec.rb
+++ spec/requests/admin_spec.rb
@@ -2,10 +2,11 @@ require 'spec_helper.rb'
 
 describe "the users list" do
   context "without login" do
+    let(:user) { FactoryGirl.create :user }
     before { visit users_path }
 
     it "denies access" do
-      page.should have_field 'user[email]'
+      page.should_not have_content user.email
     end
   end
 
@@ -13,7 +14,7 @@ describe "the users list" do
     let(:user) { FactoryGirl.create :user }
 
     before do
-      visit users_path
+      visit new_user_session_path
       fill_in 'user[email]', with: user.email
       fill_in 'user[password]', with: user.password
       click_on 'Sign in'
--- spec/requests/locations_spec.rb
+++ spec/requests/locations_spec.rb
@@ -14,7 +14,7 @@ describe "The Location list page" do
   context "when logged-in" do
     before do
       @user = FactoryGirl.create(:user)
-      visit root_url
+      visit new_user_session_path
       fill_in('user[email]', :with => @user.email) 
       fill_in('user[password]', :with => @user.password)
       click_button('Sign in')
