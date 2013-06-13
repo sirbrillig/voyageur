68fb40667430e9a066e802c0b4d4430d220cc698
Payton Swick <payton@foolord.com>
2013-06-13 14:20:46 -0400

    Add controller actions for delete and update for triplocations

--- app/controllers/triplocations_controller.rb
+++ app/controllers/triplocations_controller.rb
@@ -10,9 +10,24 @@ class TriplocationsController < ApplicationController
   end
 
   def update
+    @triplocation = Triplocation.where(id: params[:id], user_id: current_user.id).first
+
+    respond_to do |format|
+      if @triplocation.update_attributes(params[:triplocation].slice(*Triplocation.accessible_attributes))
+        format.json { head :no_content }
+      else
+        format.json { render json: @triplocation.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
+    @triplocation = Triplocation.where(id: params[:id], user_id: current_user.id).first
+    @triplocation.destroy
+
+    respond_to do |format|
+      format.json { render json: @triplocation }
+    end
   end
 
 end
---
+++ spec/controllers/triplocationscontroller_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+
+describe TriplocationsController do
+  context "when logged-in" do
+    before do
+      @user = FactoryGirl.create(:user)
+      sign_in @user
+
+      @trip = Trip.create { |trip| trip.user = @user }
+      @loc1 = FactoryGirl.create(:home_location)
+      @loc2 = FactoryGirl.create(:work_location)
+      @triploc1 = @trip.add_location @loc1
+    end
+
+    describe "GET #show" do
+      before do
+        get :show, format: :json, id: @triploc1.id
+      end
+
+      it "shows the JSON" do
+        response.body.should eq @triploc1.to_json
+      end
+    end
+
+    describe "DELETE #destroy" do
+      it "deletes the record" do
+        expect{ delete :destroy, format: :json, id: @triploc1.id }.to change(Triplocation, :count).by(-1)
+      end
+    end
+
+    describe "PUT #update" do
+      before do
+        temp = @triploc1.dup
+        temp.position = 2
+        put :update, format: :json, id: @triploc1.id, triplocation: temp.attributes
+        @triploc1.reload
+      end
+
+      it "changes the record" do
+        @triploc1.position.should eq 2
+      end
+    end
+  end
+end
