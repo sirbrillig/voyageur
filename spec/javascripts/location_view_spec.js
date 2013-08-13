a58ddcbd75c89775288f58d8c8e0651a6db37578
Payton Swick <payton@foolord.com>
2013-08-13 10:01:53 -0400

    Fill out more of the specs for add_location_to_trip

--- spec/javascripts/location_view_spec.js
+++ spec/javascripts/location_view_spec.js
@@ -8,8 +8,10 @@ describe('Voyageur.Views.Location', function() {
 
   describe('#add_location_to_trip', function() {
     before(function() {
-      this.model = new Voyageur.Models.Location({ 'address': '10 Main Street, Burlington VT', 'title': 'Location One' });
-      this.view = new Voyageur.Views.Location({'model': this.model});
+      tloc = new Voyageur.Models.Triplocation({ 'id': 4, 'position': 1 });
+      this.trip = new Voyageur.Models.Trip({ 'id': 2, 'triplocations': [ tloc ] });
+      this.model = new Voyageur.Models.Location({ 'address': '10 Main Street, Burlington VT', 'title': 'Location One', 'id': 5 });
+      this.view = new Voyageur.Views.Location({ 'model': this.model });
       this.triplocation_spy = sinon.spy(Voyageur.Models, 'Triplocation');
       this.view.add_location_to_trip();
     });
@@ -17,10 +19,27 @@ describe('Voyageur.Views.Location', function() {
     it('creates a new triplocation', function() {
       expect(this.triplocation_spy.called).to.be.true;
     });
-    it('assigns the location to the new triplocation');
-    it('assigns the trip id to the new triplocation');
-    it('adds the new triplocation to the trip collection');
-    it('sets the new triplocation position to the end of the collection');
+
+    it('assigns the location id to the new triplocation', function() {
+      expect(this.triplocation_spy.returned(sinon.match.has('location_id', this.model.id));
+    });
+
+    it('assigns the location to the new triplocation', function() {
+      expect(this.triplocation_spy.returned(sinon.match.has('location', this.model));
+    });
+
+    it('assigns the trip id to the new triplocation', function() {
+      expect(this.triplocation_spy.returned(sinon.match.has('trip_id', this.trip.id));
+    });
+
+    it('adds the new triplocation to the trip collection', function() {
+      expect(this.trip.triplocations).to.include(this.triplocation_spy.returnValues[0]);
+    });
+
+    it('sets the new triplocation position to the end of the list', function() {
+      expect(this.triplocation_spy.returned(sinon.match.has('position', 2));
+    });
+
     it('sends the trip model to the server');
     it('updates the new triplocation with the id from the server');
     it('updates the trip distance from the server');
