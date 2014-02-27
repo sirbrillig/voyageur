a41a25475bef597bd27f21124d79249e50722e86
Payton Swick <payton@foolord.com>
2014-02-26 22:19:53 -0500

    Add position to Location model and acts_as_list

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130613025320) do
+ActiveRecord::Schema.define(:version => 20140227031609) do
 
   create_table "audits", :force => true do |t|
     t.integer  "auditable_id"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(:version => 20130613025320) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.integer  "user_id"
+    t.integer  "position"
   end
 
   create_table "triplocations", :force => true do |t|
