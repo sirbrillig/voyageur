c8c12ab44939e333d150c9f2d275b196731186a8
Payton Swick <payton@foolord.com>
2014-02-26 22:55:07 -0500

    Make sure Location position defaults to 0.

--- db/migrate/20140227031609_add_position_to_location.rb
+++ db/migrate/20140227031609_add_position_to_location.rb
@@ -1,5 +1,5 @@
 class AddPositionToLocation < ActiveRecord::Migration
   def change
-    add_column :locations, :position, :integer
+    add_column :locations, :position, :integer, :default => 0
   end
 end
