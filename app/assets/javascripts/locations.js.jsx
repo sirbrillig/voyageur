8ef0409a02f2be232b9fad0edda555a7880d9a0e
Payton Swick <payton@foolord.com>
2014-06-29 19:08:01 -0400

    Replace tabs with spaces.

--- app/assets/javascripts/locations.js.jsx
+++ app/assets/javascripts/locations.js.jsx
@@ -3,14 +3,14 @@
 /* globals Location */
 
 var LocationsList = React.createClass({
-	render: function() {
-		var locationNodes = this.props.locations.map( function( location ) {
-			return <Location key={location.id} id={location.id} title={location.title} address={location.address} />;
-		} );
-		return (
-			<div className="locations">
-				{locationNodes}
-			</div>
-		);
-	}
+  render: function() {
+    var locationNodes = this.props.locations.map( function( location ) {
+      return <Location key={location.id} id={location.id} title={location.title} address={location.address} />;
+    } );
+    return (
+      <div className="locations">
+        {locationNodes}
+      </div>
+    );
+  }
 });
