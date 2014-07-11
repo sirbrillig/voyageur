4632924f21b28d5c6d03b4cdbf570940ce8c1b46
Payton Swick <payton@foolord.com>
2014-07-11 10:25:45 -0400

    Add visuals for dragging triplocations.

--- app/assets/javascripts/triplocation.js.jsx
+++ app/assets/javascripts/triplocation.js.jsx
@@ -10,20 +10,35 @@ var Triplocation = (function() { //jshint ignore:line
 
     displayName: 'Triplocation',
 
+    getInitialState: function() {
+      return { moving: false, movingOver: false };
+    },
+
     startDrag: function( evt ) {
       log('drag started', this.props.id);
       var dataTransfer = evt.nativeEvent.dataTransfer;
       dataTransfer.effectAllowed = 'move';
       dataTransfer.setData( 'text/plain', this.props.location.title );
       dataTransfer.setData( 'draggedItem', this.props.id );
+      this.setState( { moving: true } );
     },
 
     dragOver: function( evt ) {
       evt.preventDefault();
+      this.setState( { movingOver: true } );
+    },
+
+    dragLeave: function() {
+      this.setState( { movingOver: false } );
+    },
+
+    dragEnd: function() {
+      this.setState( { moving: false, movingOver: false } );
     },
 
     drop: function( evt ) {
       var draggedId = parseInt( evt.dataTransfer.getData( 'draggedItem' ), 10 );
+      this.setState( { moving: false, movingOver: false } );
       if ( draggedId === this.props.id ) return;
       log('drag dropped from', draggedId, 'to', this.props.id );
       emitter.emit( 'moveTriplocation', { from: draggedId, to: this.props.id } );
@@ -35,7 +50,14 @@ var Triplocation = (function() { //jshint ignore:line
 
     render: function() {
       return (
-        <li className="location_block" data-triplocation-id={this.props.id} draggable="true" onDragStart={this.startDrag} onDragOver={this.dragOver} onDrop={this.drop}>
+        <li className={"triplocation location_block" + ( this.state.movingOver ? ' moving-over' : '' ) + ( this.state.moving ? ' moving' : '' )}
+          data-triplocation-id={this.props.id}
+          draggable="true"
+          onDragStart={this.startDrag}
+          onDragOver={this.dragOver}
+          onDragLeave={this.dragLeave}
+          onDragEnd={this.dragEnd}
+          onDrop={this.drop} >
           <div className="location">
             <nav className="actions">
               <a className="btn btn-small remove-button" onClick={this.removeFromTrip}>
